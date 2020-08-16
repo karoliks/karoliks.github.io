864e8cf22823f0341e8e5fc9a39ee98b9a554b90
karoliks <karoline.k.sabo@gmail.com>
2020-08-16 11:52:21 +0200

    Updates

---
+++ precache-manifest.9a6017b42d38f7ce59f391269770a4cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199e2bc838bfac59d7e7f8f286309030",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2b845b20eaeb6d3a9b4",
+    "url": "./static/css/main.e1a5a4e7.chunk.css"
+  },
+  {
+    "revision": "ce167e4486e24dd97d7f",
+    "url": "./static/js/2.bd31f7b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.bd31f7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b845b20eaeb6d3a9b4",
+    "url": "./static/js/main.65ebe14f.chunk.js"
+  },
+  {
+    "revision": "08c4bf5be2a6cd0bf5db",
+    "url": "./static/js/runtime-main.efd2003d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
