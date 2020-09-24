e860d23042eeed3151775d2a62b9f4835e23d9cd
karoliks <karoline.k.sabo@gmail.com>
2020-09-24 16:31:51 +0200

    Update kproject decriptions

--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -3,14 +3,14 @@ export const projectdata = {
     img: "/projectvisuals/recipe-app.png",
     title: "Recipe Finder",
     desc:
-      "Simple display and search for recipes. Created as a way to learn React.",
+      "Simple way to display and search for recipes. Created as a way to learn React.",
     link: "https://karoliks.github.io/recipe-app/",
   },
   exp: {
     img: "/projectvisuals/exp.png",
     title: "Gallery",
     desc:
-      "Page to show of earlier creations. Made to test out smooth scroll functions in JavaScript",
+      "A place to display some drawings I made a while back. Made to test out smooth scroll functions in JavaScript",
     link: "https://karoliks.github.io/experimenting-with-smooth-scroll/",
   },
   afk: {
@@ -24,7 +24,7 @@ export const projectdata = {
     img: "/projectvisuals/Screenshot from 2020-08-16 17-39-17.png",
     title: "RPG Game",
     desc:
-      "RPG game made with React. Made to gain a deeper understanding of React  and Redux. (And beacause I wanted to make pixel art)",
+      "RPG game made with React. Made to gain a deeper understanding of React and Redux (and beacause I wanted to make pixel art).",
     link: "https://karoliks.github.io/react-game/",
   },
 };
